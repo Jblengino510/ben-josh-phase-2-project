7829c7989a39a448a5797e5ea31d74741d1ce941
benjamin <benjamin.olmsted@gmail.com>
2021-08-02 15:08:54 -0700

    Post list fetches posts and PostCard displays them

--- ben-josh-app/src/PostDetails.js
+++ ben-josh-app/src/PostDetails.js
@@ -1,17 +1,21 @@
 import { Button, Icon } from 'semantic-ui-react'
 
-function PostDetails(){
+function PostDetails({post}){
+    //fetch the comments for the post
+    //https://react.semantic-ui.com/views/card/#types-card-props
+
     return (
         <div>
-            <h1>Title</h1>
-            <img src="" alt="Some Image"/>
-            <p>Post content</p>
+            <h3>{post.title}</h3>
+            <img src={post.image} alt="Some Image"/>
+            <p>{post.text}</p>
             <ul>
                 <li>List of comments</li>
             </ul>
             <Button icon>
                 <Icon name='angle up' />
             </Button>
+            <span>{post.upvotes-post.downvotes}</span>
             <Button icon>
                 <Icon name='angle down' />
             </Button>
