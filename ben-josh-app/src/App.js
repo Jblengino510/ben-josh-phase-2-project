e640b2c53683e1331944d6c94a796b5cff14d49e
benjamin <benjamin.olmsted@gmail.com>
2021-08-02 12:49:04 -0700

    createPostForm in on the page

--- ben-josh-app/src/App.js
+++ ben-josh-app/src/App.js
@@ -21,6 +21,7 @@ function App() {
         
       </Switch>
       <PostList />
+      <CreatePostForm />
     </div>
   );
 }
--- ben-josh-app/src/CreatePostForm.js
+++ ben-josh-app/src/CreatePostForm.js
@@ -1,8 +1,14 @@
-
+import {useState, useEffect} from 'react'
+import {Input} from "semantic-ui-react"
 
 function CreatePostForm(){
     return (
-        null
+        <form>
+            <input type='text' placeholder='title'></input>
+            <input type='text' placeholder='image url'></input>
+            <input type='textarea' placeholder='Text (optional)'></input>
+            <input type='submit' value='post'></input>
+        </form>
     )
 }
 
