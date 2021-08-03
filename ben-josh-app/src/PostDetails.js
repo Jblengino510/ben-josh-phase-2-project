e2b2acdcb290c76576a6abd4f6dc4a38f2774a52
Joshua <j.blengino@yahoo.com>
2021-08-03 09:11:55 -0700

    upVote/downVote feature added

--- ben-josh-app/src/PostDetails.js
+++ ben-josh-app/src/PostDetails.js
@@ -1,8 +1,45 @@
+import { useState } from "react"
 import { Button, Icon } from 'semantic-ui-react'
 
 function PostDetails({ post }){
     //fetch the comments for the post
     //https://react.semantic-ui.com/views/card/#types-card-props
+    const [ voteCount, setVoteCount ] = useState(post.upvotes-post.downvotes)
+    const [ downVoteClicked, setDownVoteClicked ] = useState(false)
+    const [ upVoteClicked, setUpVoteClicked ] = useState(false)
+
+    function handleDownVote(){
+        if (downVoteClicked === false){
+             fetch(`http://localhost:3001/posts/${post.id}`, {
+                 method: "PATCH",
+                 headers: {'Content-Type': 'application/json'},
+                 body: JSON.stringify({
+                     downvotes: post.downvotes +1
+                 })
+             })
+             .then(res => res.json())
+             .then(data => setVoteCount(data.upvotes - data.downvotes))
+        }
+        setDownVoteClicked(downVoteClicked => !downVoteClicked)
+        setUpVoteClicked(false)
+    }
+
+    function handleUpVote(){
+        if (upVoteClicked === false){
+            fetch(`http://localhost:3001/posts/${post.id}`, {
+                method: "PATCH",
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({
+                    upvotes: post.upvotes + 1
+                })
+            })
+            .then(res => res.json())
+            .then(data => setVoteCount(data.upvotes - data.downvotes))
+       }
+        setUpVoteClicked(upVoteClicked => !upVoteClicked)  
+        setDownVoteClicked(false)  
+    }
+
 
     return (
         <div>
@@ -12,11 +49,11 @@ function PostDetails({ post }){
             <ul>
                 <li>{post.comments.length} comments</li>
             </ul>
-            <Button icon>
+            <Button icon name="upVote" onClick={handleUpVote}>
                 <Icon name='angle up' />
             </Button>
-            <span>{post.upvotes-post.downvotes}</span>
-            <Button icon>
+            <span>{voteCount}</span>
+            <Button icon name="downVote" onClick={handleDownVote}>
                 <Icon name='angle down' />
             </Button>
         </div>
