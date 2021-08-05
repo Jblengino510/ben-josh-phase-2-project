647208f27de986430b735551fe4b5eec96f2e8bf
benjamin <benjamin.olmsted@gmail.com>
2021-08-05 16:26:49 -0700

    whitespace in up/downvote

--- ben-josh-app/src/UpAndDownVote.js
+++ ben-josh-app/src/UpAndDownVote.js
@@ -5,7 +5,6 @@ function UpAndDownVote({ post }){
     const [ voteCount, setVoteCount ] = useState(post.upvotes - post.downvotes)
     const [ downVoteClicked, setDownVoteClicked ] = useState(false)
     const [ upVoteClicked, setUpVoteClicked ] = useState(false)
-    
 
     function patchPayload( payload ){
         fetch(`http://localhost:3000/posts/${post.id}`, {
@@ -20,14 +19,10 @@ function UpAndDownVote({ post }){
     function handleDownVote(){
         let payload = {}
         if (downVoteClicked === false){
-            payload = {
-                downvotes: post.downvotes + 1,
-                upvotes: post.upvotes
-            }
+            payload = {downvotes: post.downvotes + 1,
+                       upvotes: post.upvotes }
         }else{
-            payload = {
-                downvotes: post.downvotes
-            }
+            payload = { downvotes: post.downvotes }
         }
         patchPayload(payload)
         setDownVoteClicked(downVoteClicked => !downVoteClicked)
@@ -36,14 +31,10 @@ function UpAndDownVote({ post }){
     function handleUpVote(){
         let payload = {}
         if (upVoteClicked === false){
-            payload = {
-                    upvotes: post.upvotes + 1,
-                    downvotes: post.downvotes
-                }
+            payload = {upvotes: post.upvotes + 1,
+                       downvotes: post.downvotes}
             } else{
-            payload = {
-                upvotes: post.upvotes
-            }
+            payload = { upvotes: post.upvotes }
         }
         patchPayload(payload)
         setUpVoteClicked(upVoteClicked => !upVoteClicked)  
